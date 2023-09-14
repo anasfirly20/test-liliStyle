13d57c6383ef9f912e163a189ac20955b9637b2d
Anas Firly <anasfirly20@gmail.com>
2023-09-14 17:16:30 +0400

    adding modal

--- src/components/CardTotal.jsx
+++ src/components/CardTotal.jsx
@@ -1,6 +1,12 @@
 // utils
 import { formatNumber } from "../helpers/utils";
 
+// Components
+import CustomModal from "../components/CustomModal";
+
+// NextUI
+import { useDisclosure } from "@nextui-org/use-disclosure";
+
 export default function CardTotal({ dataItems, isCheckedAll }) {
   // Total Quantity of all items
   const totalQuantity = dataItems?.reduce((total, item) => {
@@ -12,6 +18,10 @@ export default function CardTotal({ dataItems, isCheckedAll }) {
     return total + item.price;
   }, 0);
 
+  const totalCost = totalQuantity * totalPrice;
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <section className="fixed border w-[25%] p-5 rounded-lg">
       <h1 className="font-bold text-2xl text-custom-black">
@@ -22,20 +32,28 @@ export default function CardTotal({ dataItems, isCheckedAll }) {
           Total Harga ({isCheckedAll ? totalQuantity : 0} barang)
         </p>
         <p className="text-base text-custom-gray w-[30%] text-end break-words">
-          Rp{isCheckedAll ? formatNumber(totalPrice * totalQuantity) : 0}
+          Rp{isCheckedAll ? formatNumber(totalCost) : 0}
         </p>
       </section>
       <section className="flex justify-between mt-5">
         <h3 className="font-bold text-xl text-custom-black">Total Harga:</h3>
         <h3 className="font-bold text-xl text-custom-black break-words text-end w-[40%]">
-          Rp{isCheckedAll ? formatNumber(totalPrice * totalQuantity) : 0}
+          Rp{isCheckedAll ? formatNumber(totalCost) : 0}
         </h3>
       </section>
       <section className="mt-5">
-        <button className="bg-custom-yellow py-2 rounded-lg w-full text-white font-semibold text-lg active:opacity-80">
+        <button
+          className="bg-custom-yellow py-2 rounded-lg w-full text-white font-semibold text-lg active:opacity-80"
+          onClick={onOpen}
+        >
           Beli ({isCheckedAll ? totalQuantity : 0})
         </button>
       </section>
+      <CustomModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        totalCost={totalCost}
+      />
     </section>
   );
 }
---
+++ src/components/CustomModal.jsx
@@ -0,0 +1,28 @@
+// Next UI
+import { Modal, ModalContent, ModalHeader, ModalBody } from "@nextui-org/modal";
+
+// Utils
+import { formatNumber } from "../helpers/utils";
+
+export default function App({ isOpen, onOpen, onOpenChange, totalCost }) {
+  return (
+    <>
+      <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent className="p-3">
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Total Belanja
+              </ModalHeader>
+              <ModalBody className="font-bold text-4xl">
+                Rp{formatNumber(totalCost)}
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+//
