f6b12856560cb3f5b207a2d2dea7ab17766bf233
Anas Firly <anasfirly20@gmail.com>
2023-09-14 09:21:08 +0400

    adding layout

--- src/pages/ShoppingCart/ShoppingCart.jsx
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -8,8 +8,19 @@ export default function ShoppingCart() {
   const { data } = useQuery(["cartsData"], () => getAllCarts());
 
   return (
-    <article>
-      <h1>Cart</h1>
+    <article className="min-h-screen pt-longer2 px-longer4">
+      <section>
+        <h1 className="font-bold text-2xl border-b pb-4">Cart</h1>
+        <section className="flex justify-between items-center border-b-2 w-full">
+          <section className="flex gap-2 py-4">
+            <input type="checkbox" />
+            <p className="font-semibold">Pilih semua</p>
+          </section>
+          <button type="button" className="text-blue-400">
+            Delete
+          </button>
+        </section>
+      </section>
     </article>
   );
 }
